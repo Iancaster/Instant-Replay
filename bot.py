6280876215602df9c8d35104ca7835f7fc0735c7
Iancaster <lvdawner@gmail.com>
2021-06-02 21:22:21 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -362,19 +362,19 @@ async def tutorialFunc(channel):
             you can review every command with `Ir.commandslist`, or just go back to the last menu and click the :notepad_spiral: .",
             tutorialFunc,channel)
         if reaction == '👨‍🏫':
-            await promptFunc(channel,'Examples',"""This will cover how to use the bot to replay channels. 
-            First, open the prompt with `Ir.replay`. If you call this while in a server, it'll replay from that server, 
-            but if you call `Ir.replay` from your DMs with the bot, it'll ask you for an invite so it knows what server you want. 
-            \nNext, you type out the name of the first channel you want to replay. (If you're in a server, you can also #mention it.) 
-            We'll use `general` as an example. Click the :leftwards_arrow_with_hook: after it's accepted to go back to the channel adding prompt. 
-            From there, you can add another channel, or press :leftwards_arrow_with_hook: again to go to the next step. 
-            \nFrom there, you can see the channels you have queued and :pencil2: add or :put_litter_in_its_place: remove channels. 
-            If you're happy with what you have, go on to :arrow_forward: play the next step, which is to determine how long ago you want to replay from. 
-            \nIt'll give you a lot of example as to how you can talk to it, but let's just say you want to start from `yesterday`. It'll 
-            ask you if it understood you correctly and give you the time it thinks you mean. If it did, press the :white_check_mark: and 
-            then the :leftwards_arrow_with_hook: to go back. From there, it's a list of each channel and the time you decided on. Since 
-            we only have the one channel being replayed, we only needed to specify time once-- press the :arrow_forward: to finish and begin actual playback!
-            """,tutorialFunc,channel)
+            await promptFunc(channel,'Examples',"This will cover how to use the bot to replay channels. \
+            First, open the prompt with `Ir.replay`. If you call this while in a server, it'll replay from that server, \
+            but if you call `Ir.replay` from your DMs with the bot, it'll ask you for an invite so it knows what server you want. \
+            \nNext, you type out the name of the first channel you want to replay. (If you're in a server, you can also #mention it.) \
+            We'll use `general` as an example. Click the :leftwards_arrow_with_hook: after it's accepted to go back to the channel adding prompt. \
+            From there, you can add another channel, or press :leftwards_arrow_with_hook: again to go to the next step. \
+            \nFrom there, you can see the channels you have queued and :pencil2: add or :put_litter_in_its_place: remove channels. \
+            If you're happy with what you have, go on to :arrow_forward: play the next step, which is to determine how long ago you want to replay from. \
+            \nIt'll give you a lot of example as to how you can talk to it, but let's just say you want to start from `yesterday`. It'll \
+            ask you if it understood you correctly and give you the time it thinks you mean. If it did, press the :white_check_mark: and \
+            then the :leftwards_arrow_with_hook: to go back. From there, it's a list of each channel and the time you decided on. Since \
+            we only have the one channel being replayed, we only needed to specify time once-- press the :arrow_forward: to finish and begin actual playback!\
+            ",tutorialFunc,channel)
         if reaction == '↩️':
             pass
         else:
@@ -390,19 +390,19 @@ async def examples(ctx):
         await ctx.message.delete()
     except:
         pass
-    await promptFunc(ctx.channel,'Examples',"""This will cover how to use the bot to replay channels. 
-    First, open the prompt with `Ir.replay`. If you call this while in a server, it'll replay from that server, 
-    but if you call `Ir.replay` from your DMs with the bot, it'll ask you for an invite so it knows what server you want. 
-    \nNext, you type out the name of the first channel you want to replay. (If you're in a server, you can also #mention it.) 
-    We'll use `general` as an example. Click the :leftwards_arrow_with_hook: after it's accepted to go back to the channel adding prompt. 
-    From there, you can add another channel, or press :leftwards_arrow_with_hook: again to go to the next step. 
-    \nFrom there, you can see the channels you have queued and :pencil2: add or :put_litter_in_its_place: remove channels. 
-    If you're happy with what you have, go on to :arrow_forward: play the next step, which is to determine how long ago you want to replay from. 
-    \nIt'll give you a lot of example as to how you can talk to it, but let's just say you want to start from `yesterday`. It'll 
-    ask you if it understood you correctly and give you the time it thinks you mean. If it did, press the :white_check_mark: and 
-    then the :leftwards_arrow_with_hook: to go back. From there, it's a list of each channel and the time you decided on. Since 
-    we only have the one channel being replayed, we only needed to specify time once-- press the :arrow_forward: to finish and begin actual playback!
-    """,tutorialFunc,ctx.channel)
+    await promptFunc(ctx.channel,'Examples',"This will cover how to use the bot to replay channels. \
+    First, open the prompt with `Ir.replay`. If you call this while in a server, it'll replay from that server, \
+    but if you call `Ir.replay` from your DMs with the bot, it'll ask you for an invite so it knows what server you want. \
+    \nNext, you type out the name of the first channel you want to replay. (If you're in a server, you can also #mention it.) \
+    We'll use `general` as an example. Click the :leftwards_arrow_with_hook: after it's accepted to go back to the channel adding prompt. \
+    From there, you can add another channel, or press :leftwards_arrow_with_hook: again to go to the next step. \
+    \nFrom there, you can see the channels you have queued and :pencil2: add or :put_litter_in_its_place: remove channels. \
+    If you're happy with what you have, go on to :arrow_forward: play the next step, which is to determine how long ago you want to replay from. \
+    \nIt'll give you a lot of example as to how you can talk to it, but let's just say you want to start from `yesterday`. It'll \
+    ask you if it understood you correctly and give you the time it thinks you mean. If it did, press the :white_check_mark: and \
+    then the :leftwards_arrow_with_hook: to go back. From there, it's a list of each channel and the time you decided on. Since \
+    we only have the one channel being replayed, we only needed to specify time once-- press the :arrow_forward: to finish and begin actual playback!\
+    ",tutorialFunc,ctx.channel)
     return
 
 @client.command()
